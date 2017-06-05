a1cbd9a2243a01ccc7439fc904e39dfcea0e425d
Tiago Boeing <tiagoboeing@bol.com.br>
2017-06-05 20:58:31 -0300

    Projeto interessante

---
+++ src/Interessante/Main.java
@@ -0,0 +1,33 @@
+package Interessante;
+
+public class Main {
+
+		public static void main(String[] args) throws InterruptedException {
+			while(true) {
+				System.out.println("||||||||||||||   ||||||||||||||   ||||||||||||||   ||||||||||||||                     ||||||||||||||   ||||||||||||||");
+				Thread.sleep(300L);
+				System.out.println("||||||||||||||   ||||||||||||||   ||||||||||||||   ||||||||||||||                     ||||||||||||||   ||||||||||||||");
+				Thread.sleep(300L);
+				System.out.println("||||             ||||       |||   |||     ||||||   ||||      ||||                     |||              |||           ");
+				Thread.sleep(300L);
+				System.out.println("||||             ||||       |||   |||       ||||   ||||      ||||                     |||              |||           ");
+				Thread.sleep(300L);
+				System.out.println("||||||||||||||   ||||       |||   |||        |||   ||||      ||||                     ||||||||||||||   ||||||||||||||");
+				Thread.sleep(300L);
+				System.out.println("||||||||||||||   ||||       |||   |||        |||   ||||||||||||||   |||||||||||||||   ||||||||||||||   ||||||||||||||");
+				Thread.sleep(300L);
+				System.out.println("|||||            ||||       |||   |||       ||||   ||||      ||||                                |||   |||           ");
+				Thread.sleep(300L);
+				System.out.println("|||||            ||||       |||   |||     ||||||   ||||      ||||                                |||   |||           ");
+				Thread.sleep(300L);
+				System.out.println("|||||            ||||||||||||||   ||||||||||||||   ||||      ||||                     ||||||||||||||   ||||||||||||||");
+				Thread.sleep(300L);
+				System.out.println("|||||            ||||||||||||||   ||||||||||||||   ||||      ||||                     ||||||||||||||   ||||||||||||||");
+				System.out.println();
+				System.out.println();
+				System.out.println();
+				System.out.println();
+			}
+		}
+	
+}
---
+++ src/Interessante/Main2.java
@@ -0,0 +1,32 @@
+package Interessante;
+
+public class Main2 {
+
+		public static void main(String[] args) throws InterruptedException {
+			while(true) {
+				System.out.println("||||||||||||||||       ||||      ||||||||||||||           |||||||||||||||||");
+				Thread.sleep(100L);
+				System.out.println("||||       |||||       ||||      ||||      ||||           |||           |||");
+				Thread.sleep(100L);
+				System.out.println("||||       |||||       ||||      ||||      ||||           |||           |||");
+				Thread.sleep(100L);
+				System.out.println("||||       |||||       ||||      ||||      ||||           |||           |||");
+				Thread.sleep(100L);
+				System.out.println("||||       |||||       ||||      ||||      ||||           |||||||||||||||||");
+				Thread.sleep(100L);
+				System.out.println("||||       |||||       ||||      ||||||||||||||           |||||||||||||||||");
+				Thread.sleep(100L);
+				System.out.println("||||       |||||       ||||      ||||      ||||                         |||");
+				Thread.sleep(100L);
+				System.out.println("||||       |||||       ||||      ||||      ||||                         |||");
+				Thread.sleep(100L);
+				System.out.println("||||||||||||||||       ||||      ||||      ||||           |||||||||||||||||");
+				Thread.sleep(100L);
+				System.out.println();
+				System.out.println();
+				System.out.println();
+				System.out.println();
+			}
+		}
+	
+}
